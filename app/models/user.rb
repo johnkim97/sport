46fd6bac43833601c74c3843f855975b1abe8c31
Your Name <you@example.com>
2013-08-09 10:17:26 -0500

    second commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 	belongs_to :team
 	belongs_to :player
 
-	validates :name, :presence => true
+	validates :name, :uniqueness => true
 	validates :name, :format => { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
 	has_secure_password
 end
